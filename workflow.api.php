8cd55abfedac9744b4afd9f331a51aa65c58b085
Nancy Wichmann <nan_wich@bellsouth.net>
2013-02-15 14:05:07 -0500

    #1918424 by NancyDru: More complete api doc.

--- workflow.api.php
+++ workflow.api.php
@@ -24,9 +24,40 @@ function hook_workflow($op, $old_state, $new_state, $node) {
       // But your module's Implements the workflow hook could
       // return FALSE here and veto the transition.
       break;
+
     case 'transition post':
       break;
+
     case 'transition delete':
       break;
+  }
+}
+
+/**
+ * Implements hook_workflow_operations().
+ *
+ * @param $op
+ *   'operations': Allow modules to insert their own workflow operations.
+ *   'state':  Allow modules to insert state operations.
+ * @param $workflow
+ *   The current workflow object.
+ * @param $state
+ *   The current state object.
+ */
+function hook_workflow($op, object $workflow, object $state) {
+  switch ($op) {
+    case 'operations':
+      $actions = array();
+      // The workflow_admin_ui module creates links to add a new state,
+      // edit the workflow, and delete the workflow.
+      // Your module may add to these actions.
+      return $actions;
+
+    case 'state':
+      $ops = array();
+      // The workflow_admin_ui module creates links to edit a state
+      // and delete the state.
+      // Your module may add to these operations.
+      return $ops;
   }
 }
