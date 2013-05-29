36ba88243c04e61539684bd18425a7778ec5b1e4
NancyDru <nan_wich@bellsouth.net>
2013-05-29 13:43:07 -0400

    Issue #1997242 by NancyDru: Hook to check if transition is allowed.

--- workflow.api.php
+++ workflow.api.php
@@ -8,7 +8,7 @@
  * Implements hook_workflow().
  *
  * @param $op
- *   The current workflow operation: 'transition pre' or 'transition post'.
+ *   The current workflow operation: 'transition permitted', 'transition pre' or 'transition post'.
  * @param $old_state
  *   The state ID of the current state.
  * @param $new_state
@@ -21,9 +21,16 @@
  */
 function hook_workflow($op, $old_state, $new_state, $node, $force = FALSE) {
   switch ($op) {
+    case 'transition permitted':
+      // The workflow module does nothing during this operation.
+      // This operation occurs when the list of available transitions
+      // is built. Your module's implementation could return FALSE
+      // here and disallow the presentation of the choice.
+      break;
+
     case 'transition pre':
       // The workflow module does nothing during this operation.
-      // But your module's Implements the workflow hook could
+      // But your module's implementation of the workflow hook could
       // return FALSE here and veto the transition.
       break;
 
